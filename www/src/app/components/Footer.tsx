a90f5a27491444fa4b332d19b97265ce308894fa
Zach Barnes <zbarnz@yahoo.com>
2024-06-22 20:06:52 -0600

    header and footer

---
+++ www/src/app/components/Footer.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import { Text, Container, ActionIcon, Group, rem } from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons-react";
+import styles from "../../styles/components/Footer.module.css";
+import { Logo } from "../../../public/icons";
+import { usePathname } from "next/navigation";
+
+const data = [
+  {
+    title: "About",
+    links: [
+      { label: "Features", link: "#" },
+      { label: "Pricing", link: "#" },
+      { label: "Support", link: "#" },
+      { label: "Forums", link: "#" },
+    ],
+  },
+  {
+    title: "Project",
+    links: [
+      { label: "Contribute", link: "#" },
+      { label: "Media assets", link: "#" },
+      { label: "Changelog", link: "#" },
+      { label: "Releases", link: "#" },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      { label: "Join Discord", link: "#" },
+      { label: "Follow on Twitter", link: "#" },
+      { label: "Email newsletter", link: "#" },
+      { label: "GitHub discussions", link: "#" },
+    ],
+  },
+];
+
+export function Footer() {
+  const pathname = usePathname();
+
+  const excludeNavPages = ["/login", "register"];
+
+  if (excludeNavPages.includes(pathname)) return null;
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text<"a">
+        key={index}
+        className={styles.link}
+        component="a"
+        href={link.link}
+        onClick={(event) => event.preventDefault()}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={styles.wrapper} key={group.title}>
+        <Text className={styles.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={styles.footer}>
+      <Container className={styles.inner}>
+        <div className={styles.logo}>
+          <Logo />
+          <Text size="xs" c="dimmed" className={styles.description}>
+            Build fully functional accessible web applications faster than ever
+          </Text>
+        </div>
+        <div className={styles.groups}>{groups}</div>
+      </Container>
+      <Container className={styles.afterFooter}>
+        <Text c="dimmed" size="sm">
+          © 2020 mantine.dev. All rights reserved.
+        </Text>
+
+        <Group
+          gap={0}
+          className={styles.social}
+          justify="flex-end"
+          wrap="nowrap"
+        >
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandTwitter
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandYoutube
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+          <ActionIcon size="lg" color="gray" variant="subtle">
+            <IconBrandInstagram
+              style={{ width: rem(18), height: rem(18) }}
+              stroke={1.5}
+            />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
